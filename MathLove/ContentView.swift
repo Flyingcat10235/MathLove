b65323a83170fa239b71b83ba538d823a84bbd28
Flyingcat10235 <honcivi@gmail.com>
2026-02-28 19:51:56 +0800

    Initial Commit

---
+++ MathLove/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathLove
+//
+//  Created by 吳一輪 on 2026/2/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathLove/MathLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathLoveApp.swift
+//  MathLove
+//
+//  Created by 吳一輪 on 2026/2/28.
+//
+
+import SwiftUI
+
+@main
+struct MathLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
